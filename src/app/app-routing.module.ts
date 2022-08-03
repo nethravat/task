79d6cac781fa5c5633642a6b4d2a9cffb44fd018
nethravathi <nethrav79@gmail.com>
2022-08-03 16:43:31 +0530

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+
+  {
+    path: 'movies',
+    loadChildren: () => import('./components/movies/movies.module').then(mod => mod.MoviesModule)
+  },
+
+  {
+    path: 'movies/:id', loadChildren: () => import('./components/movie-details/movie-details.module').then(mod => mod.MovieDetailsModule)
+  },
+
+
+
+ 
+
+  {
+    path: '**',
+    redirectTo: ''
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
